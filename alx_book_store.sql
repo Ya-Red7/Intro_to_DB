9339e83dc3ae91d89b1f710fca0a90af1ef1eb69
Yared W <yaredwondatir77@gmail.com>
2024-10-18 13:23:21 +0300

    GOOD TOGO

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,33 +1,33 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 CREATE TABLE Books (
-    book_id  int Primary Key ,
+    book_id  int PRIMARY KEY ,
     title VARCHAR(130),
-    author_id int Foreign Key ,
+    author_id int FOREIGN KEY ,
     price FLOAT,
     publication_date DATE,
     REFERENCES Authors(author_id)
 );
 CREATE TABLE Authors(
-    author_id int Primary Key ,
+    author_id int PRIMARY KEY ,
     author_name VARCHAR(215)
 );
 CREATE TABLE Customers(
-    customer_id int Primary Key ,
+    customer_id int PRIMARY KEY ,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 CREATE TABLE Orders(
-    order_id int Primary Key ,
-    customer_id int Foreign Key ,
+    order_id int PRIMARY KEY ,
+    customer_id int FOREIGN KEY ,
     order_date DATE,
     REFERENCES Customers(customer_id)
 );
 CREATE TABLE Order_Details(
-    orderdetailid int Primary Key ,
-    order_id Foreign Key,
-    book_id Foreign Key,
+    orderdetailid int PRIMARY KEY ,
+    order_id FOREIGN KEY,
+    book_id FOREIGN KEY,
     quantity FLOAT,
     REFERENCES Orders(order_id),
     REFERENCES Books(book_id)
