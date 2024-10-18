ed654f72888eb04fd356fd8a3663527328da3569
Yared W <yaredwondatir77@gmail.com>
2024-10-18 13:19:15 +0300

    fix smtn

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,34 +1,35 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 CREATE TABLE Books (
-    book_id  int Primary Key
-    title VARCHAR(130)
-    author_id int Foreign Key
-    price FLOAT
-    publication_date DATE
+    book_id  int,
+    title VARCHAR(130),
+    author_id int Foreign Key ,
+    price FLOAT,
+    publication_date DATE,
+    Primary Key (book_id),
     REFERENCES Authors(author_id)
 );
 CREATE TABLE Authors(
-    author_id int Primary Key
+    author_id int Primary Key ,
     author_name VARCHAR(215)
 );
 CREATE TABLE Customers(
-    customer_id int Primary Key
-    customer_name VARCHAR(215)
-    email VARCHAR(215)
+    customer_id int Primary Key ,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
     address TEXT
 );
 CREATE TABLE Orders(
-    order_id int Primary Key
-    customer_id int Foreign Key
-    order_date DATE
+    order_id int Primary Key ,
+    customer_id int Foreign Key ,
+    order_date DATE,
     REFERENCES Customers(customer_id)
 );
 CREATE TABLE Order_Details(
-    orderdetailid int Primary Key
-    order_id Foreign Key
-    book_id Foreign Key
-    quantity FLOAT
-    REFERENCES Orders(order_id)
+    orderdetailid int Primary Key ,
+    order_id Foreign Key,
+    book_id Foreign Key,
+    quantity FLOAT,
+    REFERENCES Orders(order_id),
     REFERENCES Books(book_id)
 );
