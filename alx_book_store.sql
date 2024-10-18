57b35fde9fe09cf987eedd3e07f67312b117b578
Yared W <yaredwondatir77@gmail.com>
2024-10-18 13:28:46 +0300

    fix smtn30

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,7 +28,7 @@ CREATE TABLE Order_Details(
     orderdetailid int PRIMARY KEY ,
     order_id INT,
     book_id INT,
-    quantity FLOAT,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
